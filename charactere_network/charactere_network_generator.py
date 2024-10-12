9d6110aafcf11807ba69dcbc4cb4459824660a86
BOUAZIZ-Dhia-Eddine <dhiabouaziz8@gmail.com>
2024-10-13 00:32:48 +0200

    update5

--- charactere_network/charactere_network_generator.py
+++ charactere_network/charactere_network_generator.py
@@ -1,50 +1,48 @@
-from pyvis.network import network 
-import networkx as nx 
-import pandas as pd 
+from pyvis.network import network
+import networkx as nx
+import pandas as pd
 
 class Charactere_network_generator():
     def __init__(self):
         pass
-    
-    def calcul_occurrences(self,df,window=10):
 
+    def calcul_occurrences(self, df, window=10):
         combinaison = {}
 
         for ligne in df['ners']:
-            ligne = [elem for item in ligne if item  for elem in item]
+            ligne = [elem for item in ligne if item for elem in item]
             for i in range(len(ligne)):
                 j = i - window
-                if i == (window-1):
-                    for k in range(0,window-1):
-                        if ligne[k]!=ligne[k+1]:
-                            if k+1 < window-1 :
-                                x=tuple(sorted([ligne[k],ligne[k+1]]))
+                if i == (window - 1):
+                    for k in range(0, window - 1):
+                        if ligne[k] != ligne[k + 1]:
+                            if k + 1 < window - 1:
+                                x = tuple(sorted([ligne[k], ligne[k + 1]]))
                                 print(x)
                                 if x in combinaison:
-                                    combinaison[x]+=1
+                                    combinaison[x] += 1
                                 else:
-                                    combinaison[x]=1
+                                    combinaison[x] = 1
                 if j >= 0:
                     l = ligne[j:i]
-
                     for k in range(len(l) - 1):
                         if l[k] != l[-1]:
                             x = tuple(sorted([l[k], l[-1]]))
                             if x in combinaison:
-                                    combinaison[x] += 1
+                                combinaison[x] += 1
                             else:
-                                    combinaison[x] = 1
-                                    
+                                combinaison[x] = 1
+
         data_tuples = [(key[0], key[1], value) for key, value in combinaison.items()]
         df = pd.DataFrame(data_tuples, columns=['Person1', 'Person2', 'Occurrences'])
-        df= df.sort_values(by='Occurrences', ascending=False)
+        df = df.sort_values(by='Occurrences', ascending=False)
         df.reset_index(drop=True, inplace=True)
-        df=df.head(200)
-        
+        df = df.head(200)
+
         return df
-    
-    def draw_graph(self,df):
-        l=nx.from_pandas_edgelist(
+
+    def draw_graph(self, df):
+        l = nx.from_pandas_edgelist(
             df,
             source='Person1',
             target='Person2',
@@ -52,13 +50,13 @@ class Charactere_network_generator():
             edge_weight='Occurrences',
             create_using=nx.Graph()
         )
-        node_degree =dict(l.degree)
-        nx.set_node_attributes(l,node_degree,"size")
-        net=network(notebook=True,width="1000px",height="700px",font_color="white",cdn_ressource="remote")
+        node_degree = dict(l.degree)
+        nx.set_node_attributes(l, node_degree, "size")
+        net = network(notebook=True, width="1000px", height="700px", font_color="white", cdn_ressource="remote")
         net.from_nx(l)
-        html=net.generate_html()
-        html=html.replace("\n"," ")
-        output_html=output_html = f"""
+        html = net.generate_html()
+        html = html.replace("\n", " ")
+        output_html = f"""
                 <iframe style="width: 100%; height: 600px; margin:0 auto" name="result" allow="midi; geolocation; microphone; camera; 
                 display-capture; encrypted-media;" sandbox="allow-modals allow-forms 
                 allow-scripts allow-same-origin allow-popups 
--- charactere_network/name_entity_recognizer.py
+++ charactere_network/name_entity_recognizer.py
@@ -2,56 +2,47 @@ import spacy
 from nltk.tokenize import sent_tokenize
 import os
 import sys
-import pandas as pd 
+import pandas as pd
 import pkg_resources
-import pathlib 
-folder_path = pathlib.Path().parent.resolve()  
+import pathlib
+
+folder_path = pathlib.Path().parent.resolve()
 sys.path.append(os.path.join(folder_path, "../"))
 
-from utils import load_substiles_dataset 
+from utils import load_substiles_dataset
 from ast import literal_eval
 
 class NameEntityRecognizer:
-    def __init__(self,path_data='../data/Subtitles/' ):
-        self.path=path_data
-        self.model=self.load_model()
-    
+    def __init__(self, path_data='../data/Subtitles/'):
+        self.path = path_data
+        self.model = self.load_model()
+
     def load_model(self):
         nlp = spacy.load("en_core_web_trf")
         return nlp
-        
-    
-    def name_recognition(self,data):
-        sentences=sent_tokenize(data)
-        list_names=list()
-        for token in sentences :
-            names=set()
-            output=self.model(token)
-            for doc in output.ents :
-                if doc.label_=='PERSON':
-                names.add(doc.text.upper())
-            list_names.append(names) 
-        return list_names
-    
-    
-    def get_ners(self,save_path=None):
-        
-        if save_path is not  None and os.path.exists(save_path):
-             df=pd.read_csv(save_path)
-             df['ners'] = df['ners'].apply(lambda x: literal_eval(x) if isinstance(x, str) else x)
 
-             return df 
-         
-        df=load_substiles_dataset(self.path)
-        df['ners']=df['script'].apply(self.name_recognition)
-        
-        if save_path is not None :
-             df.to_csv(save_path,index=False)
-             print(f'Data saved in {save_path}')
+    def name_recognition(self, data):
+        sentences = sent_tokenize(data)
+        list_names = list()
+        for token in sentences:
+            names = set()
+            output = self.model(token)
+            for doc in output.ents:
+                if doc.label_ == 'PERSON':
+                    names.add(doc.text.upper())  # Indentation corrected here
+            list_names.append(names)
+        return list_names
+
+    def get_ners(self, save_path=None):
+        if save_path is not None and os.path.exists(save_path):
+            df = pd.read_csv(save_path)
+            df['ners'] = df['ners'].apply(lambda x: literal_eval(x) if isinstance(x, str) else x)
+            return df
+
+        df = load_substiles_dataset(self.path)
+        df['ners'] = df['script'].apply(self.name_recognition)
+
+        if save_path is not None:
+            df.to_csv(save_path, index=False)
+            print(f'Data saved in {save_path}')
         return df
-        
-        
-    
-    
-            
-        
